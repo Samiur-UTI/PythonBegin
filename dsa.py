241ff433e88283bc2f2fe178845f4b103466ccb1
Ruimas-UTI <allpraisethelord@gmail.com>
2021-06-14 01:27:36 +0600

    Hash table done

--- dsa.py
+++ dsa.py
@@ -1,31 +1,43 @@
-# #Print all unique pairs in an array 
-# arr = [1,2,3,4,5,6]
-# def pairs(li):
-#   for i in li: 
-#     for j in li:
-#         if i != j:
-#             print(f"{i},{j}")
-# pairs(arr)
-# #Reverse a string
-# string = 'Akak ihsihS';
-# def reverser(str):
-#   result = ''
-#   for i in str[::-1]:
-#     result += i 
-#   return result
-# print(reverser(string))
-# #Merge sorted arrays
-# dataA = [1,2,3,4,5,6,7,8,9]
-# dataB = [21,34,53,56]
-# def sorter(a, b):
-#     new_list = a+b
-#     return sorted(new_list)
-# print(sorter(dataA, dataB))
-##Hash Tables
-table = {
-    'name': 'Samiur',
-    'age': 36,
-    'gender': 'Male',
-    'power' : lambda x:x*5 
-}
-print(table)
+#Print all unique pairs in an array 
+arr = [1,2,3,4,5,6]
+def pairs(li):
+  for i in li: 
+    for j in li:
+        if i != j:
+            print(f"{i},{j}")
+pairs(arr)
+#Reverse a string
+string = 'ihsihS Akak';
+def reverser(str):
+  result = ''
+  for i in str[::-1]:
+    result += i 
+  return result
+print(reverser(string))
+#Merge sorted arrays
+dataA = [1,2,3,4,5,6,7,8,9]
+dataB = [21,34,53,56]
+def sorter(a, b):
+    new_list = a+b
+    return sorted(new_list)
+print(sorter(dataA, dataB))
+#Hash Tables
+class HashTable():
+    def __init__(self):
+        self.data = []
+        print(self.data)
+    def set(self,key,value):
+        temp = []
+        temp.append(key)
+        temp.append(value)
+        self.data.append(temp)
+    def get(self,key):
+        for items in self.data:
+            for item in items:
+                if item == key:
+                    return items
+                else: return 'not found'
+
+
+                    
+                
--- interview.py
+++ interview.py
@@ -1,4 +1,4 @@
-#Codeforces problem
+Codeforces problem
 charm = input('Enter a nice array')
 arr = [char for char in charm.split()]
 print(list(map(lambda x: int(x, 10) , arr)))
@@ -6,3 +6,23 @@ print(list(map(lambda x: int(x, 10) , arr)))
 tup = [('lulu',25),('Prince',0),('Rafi',50)]
 pimp = {key:value for key,value in tup}
 print(pimp)
+#Google interview question
+array = [2,5,1,2,3,5,1,2]
+array2 = [3,5,4,9,3,7,5]
+def reccurance(array):
+    for items in array:
+        for i in array:
+            if i == items:
+                return i
+def reccurance2 (arr):
+    x = {}
+    for i in arr:
+        if i in x:
+            return i
+        x[i] = 1
+    return 'nothing found'
+    
+
+
+print(reccurance2([3,2,2,4]))        
+            
