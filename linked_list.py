af0d89d99a69db6894e5d57ff600cd50d841ff75
Ruimas-UTI <allpraisethelord@gmail.com>
2021-07-21 04:34:32 +0600

    insert added

--- linked_list.py
+++ linked_list.py
@@ -17,12 +17,31 @@ class LinkedList ():
         newNode.next = self.head
         self.head = newNode
         self.length += 1
+    def traverse(self,index):
+        counter = 0
+        currentNode = self.head
+        while(counter != index):
+            currentNode = currentNode.next
+            counter += 1
+    def insert(self,index,value):
+        if(index >= self.length):
+            return self.append(value)
+        else: 
+            newNode = Node(value)
+            leadIndex = self.traverse(index -1)
+            nextIndex = leadIndex.next
+            leadIndex.next = newNode
+            newNode.next = nextIndex
+            self.length += 1
+            return self.printList
     def printList(self):
         temp = self.head
+        arr = []
         while (temp):
-            print (temp.value)
+            arr.append(temp.value)
             temp = temp.next
-
+        print(arr)
+    
 alpha = LinkedList(2)
 alpha.append(10)
 
